a8230ac8c3e9aad5b2d04ed3d70e5808f2baa3cd
WojtekZieziula <wojciech.zieziula@gmail.com>
2025-12-11 10:39:07 +0100

    implemented get_file_info() function

--- file_organizer.py
+++ file_organizer.py
@@ -1,5 +1,8 @@
 import os
+import sys
 import configparser
+import stat
+
 
 def load_config():
     config_path = os.path.expanduser("~/.clean_files")
@@ -19,10 +22,32 @@ def load_config():
 
     return defaults
 
+
 CONFIG = load_config()
 
-print(CONFIG.get('permissions'))
-print(CONFIG.get('bad_chars'))
-print(CONFIG.get('temp_extensions'))
-print(CONFIG.get('bad_char_sub'))
 
+def get_file_info(filepath):
+    """Gathers file metadata"""
+    try:
+        st = os.stat(filepath)
+        return {
+            'path': filepath,
+            'name': os.path.basename(filepath),
+            'size': st.st_size,
+            'mtime': st.st_mtime,
+            'mode': stat.S_IMODE(st.st_mode),
+            'hash': None
+        }
+    except OSError:
+        return None
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python file_organizer.py <directory_X> [directory_Y1] ...")
+        sys.exit(1)
+
+    target_dir = sys.argv[1]
+    other_dirs = sys.argv[1:]
+
+    print(get_file_info(target_dir))
